2621ec6b577d68eb7d253eac8b55fd06fc4dd04b
Tom Sherman <tompsherman@gmail.com>
2020-09-16 16:36:20 -0400

    name email pass

--- README.md
+++ README.md
@@ -20,9 +20,9 @@ We've seen many different styles of form management by now -- simple to complex.
 
 We want to create a form to onboard a new user to our system. We need _at least_ the following pieces of information about our new user:
 
-- [ ] Name
-- [ ] Email
-- [ ] Password
+- [x] Name
+- [x] Email
+- [x] Password
 - [ ] Terms of Service (checkbox)
 - [ ] A Submit button to send our form data to the server.
 
--- afternoon-project/src/Form.js
+++ afternoon-project/src/Form.js
@@ -1,10 +1,77 @@
-import React from "react"
+import React, {useState} from "react"
+import cuid from "cuid"
+
+const initialFormValues = ({
+    name: "",
+    email: "",
+    password: "",
+})
 
 function Form(){
+
+const [users, setUsers] = useState([])
+const [formValues, setFormValues] = useState(initialFormValues)
+
+const onChange = ((event) => {
+    const {name, value} = event.target
+    setFormValues({...formValues, [name]: value})
+})
+
+const onSubmit = ((event)=>{
+    event.preventDefault();
+    const newUser = {
+        name: formValues.name.trim(),
+        email: formValues.email.trim(),
+        password: formValues.password.trim(),
+    }
+    setUsers([...users, newUser])
+    setFormValues(initialFormValues)
+})
     return (
+    <>
     <div>
         Hello worlds
+    
+    {
+        users.map((user)=>{
+           return (
+            <div key={cuid()}>  
+               <p>name: {user.name}</p>
+               <p>email: {user.email}</p>
+               <p>password: {user.password}</p>
+               <br></br>
+            </div>
+ 
+            )
+        })
+    }
+    <form onSubmit={onSubmit}>
+        <input 
+            name="name"
+            type="text"
+            value={users.name}
+            onChange={onChange}
+            placeholder="enter name"
+        />
+        <input 
+            name="email"
+            type="email"
+            value={users.email}
+            onChange={onChange}
+            placeholder="enter email"
+        />
+        <input 
+            name="password"
+            type="text"
+            value={users.password}
+            onChange={onChange}
+            placeholder="enter password"
+        />
+        <button>submit</button>
+    </form>
+    
     </div>
+    </>
     )
 }
 
